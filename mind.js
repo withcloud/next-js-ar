e996a87880218c2df1465151373c212962e83c7e
comus <comus.leong@gmail.com>
2022-03-07 14:46:01 +0800

    update

---
+++ mind.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+import 'mindar-image-three.js'
+
+const Page = () => {
+  useEffect(() => {
+
+  })
+  return (
+    <div>dsjflksdjfljsdlf</div>
+  )
+}
+
+export default Page
